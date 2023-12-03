145571b9745612e29740fdfb2f9e3bc6d0b9296a
Archita Dubey <148361864+Archita13Mystic@users.noreply.github.com>
2023-12-03 15:21:59 +0530

    Add files via upload

---
+++ Recursion.py
@@ -0,0 +1,28 @@
+"""def f(n):
+    if n>1:
+        return n+(f(n-1))
+    else:
+        return n
+p=int(input("enter p: "))
+print(f(p))"""
+
+
+
+"""#lambda fuction
+root_1=lambda a,b,c: (-b +((b**2-4*a*c)**0.5))/(2**a)
+root_2=lambda a,b,c: (-b -((b**2-4*a*c)**0.5))/(2**a)
+a=int(input("a: "))
+b=int(input("b: "))
+c=int(input("c: "))
+ptint(root_1(a,b,c))
+print(root_2(a,b,c))
+"""
+
+
+
+f=lambda a,b=3: a**b
+l=list(map(int(input("Enter numbers separated by $").split('$'))))
+
+
+#map (function , iterable)
+
